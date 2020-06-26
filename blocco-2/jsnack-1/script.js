3fe9f07fa8ebd65c9610d6599bfb1f5e5b5339a7
Michael Cinus <michaelcinus@gmail.com>
2020-06-26 13:42:23 +0200

    JS: JSnack1 blocco2

---
+++ blocco-2/jsnack-1/script.js
@@ -0,0 +1,11 @@
+//JSnack1
+var num1 = parseInt(prompt ("inserisci un numero"));
+var num2 = parseInt(prompt ("inserisci un numero"));
+
+if (num1 > num2) {
+    console.log(num1);
+} else if (num2 > num1) {
+    console.log(num2);
+} else {
+    console.log("i numeri sono uguali");
+}

a76531d53552b0808045c0701dc1c28c70ea7f71
Michael Cinus <michaelcinus@gmail.com>
2020-06-26 13:47:20 +0200

    JS: JSnack5

---
+++ blocco-2/jsnack-5/script.js
@@ -0,0 +1,16 @@
+//JSnack5
+var numeriDisp = [];
+var numero;
+var i = 0;
+
+while (i<6) {
+    numero = parseInt(prompt ("inserisci un numero"));
+    if (numero %2){
+        numeriDisp.push(numero);
+    }
+    i++;
+
+    console.log(numero);
+}
+
+console.log(numeri);
